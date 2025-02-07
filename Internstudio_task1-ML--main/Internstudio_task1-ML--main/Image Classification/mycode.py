0e69bbea77f39c31e0b3861434487d10cdfba112
Sujoy0705 <sujoykodali2387@gmail.com>
2025-02-07 20:04:36 +0530

    Add files via upload

---
+++ Internstudio_task1-ML--main/Internstudio_task1-ML--main/Image Classification/mycode.py
@@ -0,0 +1,46 @@
+import os
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Load pre-trained MobileNetV2 model + higher level layers
+model = tf.keras.applications.MobileNetV2(weights='imagenet', include_top=True)
+
+# Define ImageNet class labels for cats and dogs
+cat_labels = {'Egyptian_cat', 'tabby', 'Persian_cat', 'Siamese_cat', 'tiger_cat'}
+dog_labels = {'Chihuahua', 'Japanese_spaniel', 'Maltese_dog', 'Pekinese', 'Shih-Tzu', 'Blenheim_spaniel', 'papillon', 'toy_terrier', 'Rhodesian_ridgeback', 'Afghan_hound', 'basset', 'beagle', 'bloodhound', 'bluetick', 'black-and-tan_coonhound', 'Walker_hound', 'English_foxhound', 'redbone', 'borzoi', 'Irish_wolfhound', 'Italian_greyhound', 'whippet', 'Ibizan_hound', 'Norwegian_elkhound', 'otterhound', 'Saluki', 'Scottish_deerhound', 'Weimaraner', 'Staffordshire_bullterrier', 'American_Staffordshire_terrier', 'Bedlington_terrier', 'Border_terrier', 'Kerry_blue_terrier', 'Irish_terrier', 'Norfolk_terrier', 'Norwich_terrier', 'Yorkshire_terrier', 'wire-haired_fox_terrier', 'Lakeland_terrier', 'Sealyham_terrier', 'Airedale', 'cairn', 'Australian_terrier', 'Dandie_Dinmont', 'Boston_bull', 'miniature_schnauzer', 'giant_schnauzer', 'standard_schnauzer', 'Scotch_terrier', 'Tibetan_terrier', 'silky_terrier', 'soft-coated_wheaten_terrier', 'West_Highland_white_terrier', 'Lhasa', 'flat-coated_retriever', 'curly-coated_retriever', 'golden_retriever', 'Labrador_retriever', 'Chesapeake_Bay_retriever', 'German_short-haired_pointer', 'vizsla', 'English_setter', 'Irish_setter', 'Gordon_setter', 'Brittany_spaniel', 'clumber', 'English_springer', 'Welsh_springer_spaniel', 'cocker_spaniel', 'Sussex_spaniel', 'Irish_water_spaniel', 'kuvasz', 'schipperke', 'groenendael', 'malinois', 'briard', 'kelpie', 'komondor', 'Old_English_sheepdog', 'Shetland_sheepdog', 'collie', 'Border_collie', 'Bouvier_des_Flandres', 'Rottweiler', 'German_shepherd', 'Doberman', 'miniature_pinscher', 'Greater_Swiss_Mountain_dog', 'Bernese_mountain_dog', 'Appenzeller', 'EntleBucher', 'boxer', 'bull_mastiff', 'Tibetan_mastiff', 'French_bulldog', 'Great_Dane', 'Saint_Bernard', 'Eskimo_dog', 'malamute', 'Siberian_husky', 'dalmatian', 'affenpinscher', 'basenji', 'pug', 'Leonberg', 'Newfoundland', 'Great_Pyrenees', 'Samoyed', 'Pomeranian', 'chow', 'keeshond', 'Brabancon_griffon', 'Pembroke', 'Cardigan'}
+
+# Function to preprocess the image
+def preprocess_image(img_path):
+    img = image.load_img(img_path, target_size=(224, 224))  # Resize image to 224x224
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
+    return img_array
+
+# Function to predict whether the image contains a cat or a dog
+def classify_image(img_path):
+    img_array = preprocess_image(img_path)
+    preds = model.predict(img_array)
+    decoded_preds = tf.keras.applications.mobilenet_v2.decode_predictions(preds, top=5)[0]  # Get top 5 predictions
+    
+    for _, label, _ in decoded_preds:
+        if label in cat_labels:
+            print("Prediction: Cat")
+            break
+        elif label in dog_labels:
+            print("Prediction: Dog")
+            break
+    else:
+        print("Prediction: Neither Cat nor Dog")
+    
+    # Display the image
+    img = image.load_img(img_path)
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
+
+# Example usage
+classify_image('path_to_test_image.jpg')
